792af5bd6b93643a40b0622874bd07a324456a04
MasameEh <samehms116@gmail.com>
2025-05-24 19:47:38 +0300

    handle tennis and basket logos from api

--- Sportify/Model/Fixture.swift
+++ Sportify/Model/Fixture.swift
@@ -36,7 +36,7 @@ struct Fixture: Decodable {
     let fkStageKey: Int?
     let stageName: String?
     let leagueGroup: String?
-    
+
     enum CodingKeys: String, CodingKey {
         case eventKey = "event_key"
         case eventDate = "event_date"
@@ -60,6 +60,8 @@ struct Fixture: Decodable {
         case eventReferee = "event_referee"
         case homeTeamLogo = "home_team_logo"
         case awayTeamLogo = "away_team_logo"
+        case eventHomeTeamLogo = "event_home_team_logo"
+        case eventAwayTeamLogo = "event_away_team_logo"
         case eventCountryKey = "event_country_key"
         case leagueLogo = "league_logo"
         case countryLogo = "country_logo"
@@ -68,9 +70,63 @@ struct Fixture: Decodable {
         case fkStageKey = "fk_stage_key"
         case stageName = "stage_name"
         case leagueGroup = "league_group"
+        case eventFirstPlayer = "event_first_player"
+        case eventSecondPlayer = "event_second_player"
+        case firstPlayerKey = "first_player_key"
+        case secondPlayerKey = "second_player_key"
+        case firstPlayerLogo = "event_first_player_logo"
+        case secondPlayerLogo = "event_second_player_logo"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        eventKey = try? container.decode(Int.self, forKey: .eventKey)
+        eventDate = try? container.decode(String.self, forKey: .eventDate)
+        eventTime = try? container.decode(String.self, forKey: .eventTime)
+        eventHomeTeam = (try? container.decode(String.self, forKey: .eventHomeTeam)) ??
+                        (try? container.decode(String.self, forKey: .eventFirstPlayer))
+        homeTeamKey = (try? container.decode(Int.self, forKey: .homeTeamKey)) ??
+                      (try? container.decode(Int.self, forKey: .firstPlayerKey))
+
+        eventAwayTeam = (try? container.decode(String.self, forKey: .eventAwayTeam)) ??
+                        (try? container.decode(String.self, forKey: .eventSecondPlayer))
+        awayTeamKey = (try? container.decode(Int.self, forKey: .awayTeamKey)) ??
+                      (try? container.decode(Int.self, forKey: .secondPlayerKey))
+        eventHalftimeResult = try? container.decode(String.self, forKey: .eventHalftimeResult)
+        eventFinalResult = try? container.decode(String.self, forKey: .eventFinalResult)
+        eventFTResult = try? container.decode(String.self, forKey: .eventFTResult)
+        eventPenaltyResult = try? container.decode(String.self, forKey: .eventPenaltyResult)
+        eventStatus = try? container.decode(String.self, forKey: .eventStatus)
+        countryName = try? container.decode(String.self, forKey: .countryName)
+        leagueName = try? container.decode(String.self, forKey: .leagueName)
+        leagueKey = try? container.decode(Int.self, forKey: .leagueKey)
+        leagueRound = try? container.decode(String.self, forKey: .leagueRound)
+        leagueSeason = try? container.decode(String.self, forKey: .leagueSeason)
+        eventLive = try? container.decode(String.self, forKey: .eventLive)
+        eventStadium = try? container.decode(String.self, forKey: .eventStadium)
+        eventReferee = try? container.decode(String.self, forKey: .eventReferee)
+
         
+        homeTeamLogo = (try? container.decode(String.self, forKey: .homeTeamLogo)) ??
+                       (try? container.decode(String.self, forKey: .eventHomeTeamLogo)) ??
+                       (try? container.decode(String.self, forKey: .firstPlayerLogo))
+
+        awayTeamLogo = (try? container.decode(String.self, forKey: .awayTeamLogo)) ??
+                       (try? container.decode(String.self, forKey: .eventAwayTeamLogo)) ??
+                       (try? container.decode(String.self, forKey: .secondPlayerLogo))
+
+        eventCountryKey = try? container.decode(Int.self, forKey: .eventCountryKey)
+        leagueLogo = try? container.decode(String.self, forKey: .leagueLogo)
+        countryLogo = try? container.decode(String.self, forKey: .countryLogo)
+        eventHomeFormation = try? container.decode(String.self, forKey: .eventHomeFormation)
+        eventAwayFormation = try? container.decode(String.self, forKey: .eventAwayFormation)
+        fkStageKey = try? container.decode(Int.self, forKey: .fkStageKey)
+        stageName = try? container.decode(String.self, forKey: .stageName)
+        leagueGroup = try? container.decode(String.self, forKey: .leagueGroup)
     }
 }
+
 
 struct Lineups: Decodable {
     let homeTeam: TeamLineup?
